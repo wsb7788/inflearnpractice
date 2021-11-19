54f2eb34bb0302a1e28c6b569ee6c50d126be860
wsb7788 <wsb0514@naver.com>
2021-11-20 00:34:11 +0900

    Initial commit

---
+++ src/main/java/com/example/inflearnpractice/InflearnpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearnpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnpracticeApplication.class, args);
+	}
+
+}
